629588c657ebe1efed30dc45714c48b49ef2483b
phuongtra06 <nguyenphuongtra171206@gmail.com>
2025-12-12 08:24:47 +0700

    feat:muontra

--- quan-li-muon-tra.py
+++ quan-li-muon-tra.py
@@ -1,13 +1,10 @@
-from modules.borrow_day import borrow_book
-from modules.return_day import return_book
-from modules.count_borrow import count_borrow_in_day
-from modules.count_return import count_return_in_day
+from datetime import datetime
 
-print("=== Quản lí mượn - trả sách ===")
+# ==========================
+# CƠ SỞ DỮ LIỆU GIẢ
+# ==========================
+borrow_records = []   # Lưu thông tin mượn – trả
 
-borrow_book("SV01", "S001", "10/12/2025", "15/12/2025")
-return_book("SV01", "S001", "14/12/2025")
 
-count_borrow_in_day("10/12/2025")
-count_return_in_day("14/12/2025")
+# ==========================
 
