65c2eef4db9b16b30edf182bc43cf180dcc86a8f
nguyen thi thuy linh <nguyenthithuylinh070906@gmail.com>
2025-12-12 09:54:56 +0700

    feat:themsach

--- quan_li_sach.py
+++ quan_li_sach.py
@@ -1,2 +1,8 @@
 # Danh sách lưu sách
 books = []
+def add_book():
+    title = input("Nhập tên sách: ")
+    author = input("Nhập tác giả: ")
+    book = {"title": title, "author": author}
+    books.append(book)
+    print("✔ Đã thêm sách\n")
