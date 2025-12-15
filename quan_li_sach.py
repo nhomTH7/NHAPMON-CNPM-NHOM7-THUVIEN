4ccc5d8c492be404d43690ad7ba6f9f52ade87d5
nguyen thi thuy linh <nguyenthithuylinh070906@gmail.com>
2025-12-15 20:27:36 +0700

    feat:xoasach

---
+++ quan_li_sach.py
@@ -0,0 +1,37 @@
+# ============================
+# CHƯƠNG TRÌNH QUẢN LÝ SÁCH
+# ============================
+
+# Mỗi quyển sách là 1 dict:
+# { "id": ..., "ten": ..., "tac_gia": ..., "nam": ..., "so_luong": ... }
+
+danh_sach_sach = [] 
+# Danh sách lưu sách
+books = []
+def add_book():
+    title = input("Nhập tên sách: ")
+    author = input("Nhập tác giả: ")
+    book = {"title": title, "author": author}
+    books.append(book)
+    print("✔ Đã thêm sách\n")
+
+
+def show_books():
+    if not books:
+        print("Không có sách nào.\n")
+        return
+    print("\nDanh sách sách:")
+    for i, b in enumerate(books, start=1):
+        print(f"{i}. {b['title']} - {b['author']}")
+    print()
+
+def delete_book():
+    show_books()
+    if not books:
+        return
+    try:
+        index = int(input("Nhập số thứ tự sách muốn xóa: "))
+        books.pop(index - 1)
+        print("✔ Đã xóa sách\n")
+    except:
+        print("❌ Số không hợp lệ.\n")
