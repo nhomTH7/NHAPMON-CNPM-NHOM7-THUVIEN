bf8dc3771aaaa7fe885708094a6a19bd1af1b72e
nguyen thi thuy linh <nguyenthithuylinh070906@gmail.com>
2025-12-16 08:19:02 +0700

    feat:xoasach

--- quan_li_sach.py
+++ quan_li_sach.py
@@ -1,2 +1,10 @@
-# Danh sách lưu sách
-books = []
+def delete_book():
+    show_books()
+    if not books:
+        return
+    try:
+        stt = int(input("Nhập số thứ tự sách cần xóa: "))
+        books.pop(stt - 1)
+        print("✔ Đã xóa sách\n")
+    except:
+        print("❌ Lỗi! Nhập sai số.\n")
